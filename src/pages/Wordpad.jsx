55c3a9cf4895304a59d0f2f9169ebebeacf755ca
Natanael Pacheco <natepacheco716@gmail.com>
2022-05-27 16:47:06 -0500

    Completed Calendar App.

--- src/pages/Wordpad.jsx
+++ src/pages/Wordpad.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { HtmlEditor, Image, Inject, Link, QuickToolbar, RichTextEditorComponent, Toolbar } from '@syncfusion/ej2-react-richtexteditor';
+
+import { EditorData } from '../data/dummy';
+import { Header } from '../components';
 
 const Wordpad = () => {
   return (
-    <div>Wordpad</div>
+    <div className='m-2 md:m-10 mt-24 p-2 md:p-10 bg-white eounded-3xl'>
+      <Header category='App' title='Wordpad' />
+      <RichTextEditorComponent id='defaultRTE' value={EditorData}>
+        <Inject services={[Toolbar, Link, Image, HtmlEditor, QuickToolbar]} />
+      </RichTextEditorComponent>
+      </div>
   )
 }
 
