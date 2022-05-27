cdabe047f16f5bcc82f99f8be91e9d507dd0d2e0
Natanael Pacheco <natepacheco716@gmail.com>
2022-05-26 23:48:48 -0500

    Fixed navbar icon bug.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,9 +18,8 @@ const NavButton = ({ title, customFunc, icon, color, dotColor}) => (
     type="button" onClick={customFunc} style={{ color }}
     className="relative text-xl rounded-full p-3 hover:bg-light-gray">
       <span>
-        <span style={{ background: dotColor }} className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2">
+        <span style={{ background: dotColor }} className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2" />
           {icon}
-        </span>
       </span>
     </button>
   </TooltipComponent>
@@ -39,6 +38,15 @@ const { activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick, screenS
 
     return () => window.removeEventListener("resize", handleResize);
   }, []);
+
+  useEffect(() => {
+    if ( screenSize <= 900) {
+      setActiveMenu(false);
+    } else {
+      setActiveMenu(true);
+    }
+  }, [screenSize]);
+
   return (
     <div className="flex justify-between p-2 md:mx-6 relative">
       <NavButton 
@@ -73,8 +81,8 @@ const { activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick, screenS
                 className="rounded-full h-8 w-8" 
                 />
                 <p>
-                <span className="text-gray-400 text-14">Hi, </span> {' '}
-                <span className="text-gray-400 text-14 font-bold ml-1">Natanael</span>
+                  <span className="text-gray-400 text-14">Hi, </span> {' '}
+                  <span className="text-gray-400 text-14 font-bold ml-1">Natanael</span>
                 </p>
                 <MdKeyboardArrowDown className="text-gray-400 text-14" />
             </div>
