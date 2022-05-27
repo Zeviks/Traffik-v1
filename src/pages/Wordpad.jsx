ee4b721d31d821ea7662073b8a1b5fbc449103f2
Natanael Pacheco <natepacheco716@gmail.com>
2022-05-26 19:23:54 -0500

    Completed Strucutre for Pages

---
+++ src/pages/Wordpad.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Wordpad = () => {
+  return (
+    <div>Wordpad</div>
+  )
+}
+
+export default Wordpad
---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+
+export { default as Dashboard } from './Dashboard';
+export { default as Employees } from './Employees';
+export { default as Calendar } from './Calendar';
+export { default as Wordpad } from './Wordpad';
+export { default as Line } from './Charts/Line';
+export { default as Inovice } from './Inovice';
+export { default as Mail } from './Mail';
